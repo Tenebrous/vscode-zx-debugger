9232f96ac4b2015c2fabfddff322c506ce81827f
Tenebrous <m@tenebrous.co.uk>
2017-09-28 14:42:59 +0100

    Tinkering with vscode API.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import * as fs     from 'fs';
+import * as path   from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,13 +12,26 @@ export function activate(context: vscode.ExtensionContext) {
 		)	
     );
 
+	// context.subscriptions.push(
+    //     vscode.debug.onDidReceiveDebugSessionCustomEvent(e => {
+    //         if( e.event == 'showHeatMap' ) {
+    //             showHeatMap( e.body );
+    //         }
+    //     })
+    // );
+
 	context.subscriptions.push(
         vscode.debug.onDidReceiveDebugSessionCustomEvent(e => {
-            if( e.event == 'showHeatMap' ) {
-                showHeatMap( e.body );
+            if( e.event == 'showDisassembly' ) {
+                showDisassembly( e.body );
             }
         })
     );
+
+    // vscode.languages.registerDefinitionProvider(
+    //     {pattern: "*.{c,asm}"},
+    //     new DefinitionProvider()
+    // );
 }
 
 function startSession(config) {
@@ -30,36 +45,53 @@ function startSession(config) {
     return { status: 'ok' };
 }
 
-var decs=[];
-function showHeatMap( data ) {
+// var decs=[];
+// function showHeatMap( data ) {
 
-    decs.forEach(dec => {
-        dec.dispose();
-    });
+//     decs.forEach(dec => {
+//         dec.dispose();
+//     });
 
-    vscode.window.visibleTextEditors.forEach( editor => {
+//     vscode.window.visibleTextEditors.forEach( editor => {
         
-        var document = editor.document;
+//         var document = editor.document;
 
-        if( document.fileName.endsWith('disasm.zdis') )
-        {
-            for (var lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
+//         if( document.fileName.endsWith('disasm.zdis') )
+//         {
+//             for (var lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
                 
-                var annotation = '';
+//                 var annotation = '';
 
-                if( (lineNumber+1) in data )
-                    annotation = '' + data[lineNumber+1];
+//                 if( (lineNumber+1) in data )
+//                     annotation = '' + data[lineNumber+1];
 
-                var dec = vscode.window.createTextEditorDecorationType({isWholeLine:true, before:{contentText: annotation, width: '150px'}});
+//                 var dec = vscode.window.createTextEditorDecorationType({isWholeLine:true, before:{contentText: annotation, width: '150px'}});
                 
-                editor.setDecorations(
-                    dec,
-                    [new vscode.Range(lineNumber,0,lineNumber,0)]
-                );
+//                 editor.setDecorations(
+//                     dec,
+//                     [new vscode.Range(lineNumber,0,lineNumber,0)]
+//                 );
 
-                decs.push( dec );
-            }
-        }
+//                 decs.push( dec );
+//             }
+//         }
 
-    });
+//     });
+// }
+
+function showDisassembly( data ) {
+    console.log( "showDisassembly: " + data );
 }
+
+// class DefinitionProvider implements vscode.DefinitionProvider {
+//     provideDefinition(
+//         document: vscode.TextDocument,
+//         position: vscode.Position,
+//         token: vscode.CancellationToken): vscode.Definition
+//     {
+//         return new vscode.Location( 
+//             vscode.Uri.file(".zxdbg\\disasm.zdis"), 
+//             new vscode.Position( 10,10 )
+//         );
+//     }
+// }
